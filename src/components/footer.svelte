a66bec08f2ff2845a10785c41382726d2db045dd
AroLeaf <starleafofa@gmail.com>
2022-03-25 11:38:50 +0100

    [feat] preview other theme icon on hover

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -1,7 +1,11 @@
 <script>
   const icons = {
     dark: 'moon',
-    light: 'sun'
+    light: 'sun',
+    hover: {
+      dark: 'sun',
+      light: 'moon',
+    }
   }
   let theme = window.localStorage.getItem('theme') || 'light';
   $: document.getElementById('themelink').href = `styles/${theme}.css`;
@@ -9,32 +13,40 @@
 </script>
 
 
-<div>
+<footer>
   <p>
     &copy; 2022
     <a href="https://github.com/msbukkuri">Swiftfyre</a>,
     <a href="https://github.com/AroLeaf">AroLeaf</a>
   </p>
   <label>
-    <span class="fa-solid fa-{icons[theme]}"></span>
+    <span class="fa-stack">
+      <span class="fa-solid fa-stack-2x fa-{icons[theme]}"></span>
+      <span class="fa-solid fa-stack-2x fa-{icons.hover[theme]} hover"></span>
+    </span>
     <input type="checkbox" checked={window.localStorage.getItem('theme') === 'dark'} on:input={(e) => theme = e.target.checked ? 'dark' : 'light'}>
   </label>
-</div>
+</footer>
 
 
 <style>
-  div {
+  footer {
     display: grid;
     grid-template-columns: auto auto;
     justify-content: space-between;
-    align-items: baseline;
-    margin-bottom: 8px;
+    align-items: end;
+    margin-bottom: 16px;
   }
 
   label {
     cursor: pointer;
-    font-size: 20px;
+    font-size: 16px;
   }
+
+  .fa-solid { transition: .2s; }
+  .fa-solid.hover{ opacity: 0; }
+  label:hover .fa-solid { opacity: 0; }
+  label:hover .fa-solid.hover { opacity: 1; }
 
   input {
     display: none;
