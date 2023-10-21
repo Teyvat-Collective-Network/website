589cb86b007893096f64636f53d9ea1032f57755
hyper-neutrino <alex.yj.liao@gmail.com>
2023-10-21 17:07:46 -0400

    remove csrf check origin

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
-import adapter from "svelte-adapter-bun";
 import { vitePreprocess } from "@sveltejs/kit/vite";
+import adapter from "svelte-adapter-bun";
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = { preprocess: vitePreprocess(), kit: { adapter: adapter() }, compilerOptions: { customElement: true } };
+const config = { preprocess: vitePreprocess(), kit: { adapter: adapter(), csrf: { checkOrigin: false } }, compilerOptions: { customElement: true } };
 export default config;
