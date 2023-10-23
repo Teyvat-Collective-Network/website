1da17241c405179f6bd468639e18ab37662b9d52
hyper-neutrino <alex.yj.liao@gmail.com>
2023-10-23 11:35:19 -0400

    sort partner list

--- src/routes/(default)/partners/+page.ts
+++ src/routes/(default)/partners/+page.ts
@@ -1,6 +1,7 @@
 import api from "$lib/api.js";
 import type { Load } from "@sveltejs/kit";
+import { Guild } from "../../../lib/types.js";
 
 export const load: Load = async () => {
-    return { partners: await api(null, `GET /guilds`) };
+    return { partners: ((await api(null, `GET /guilds`)) as Guild[]).sort((x, y) => x.name.localeCompare(y.name)) };
 };
