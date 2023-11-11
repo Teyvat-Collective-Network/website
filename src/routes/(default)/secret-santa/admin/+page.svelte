23f8fba07164249530cb31201f8a00b0d002b3ce
hyper-neutrino <alex.yj.liao@gmail.com>
2023-11-11 17:54:25 -0500

    add admin controls

--- src/routes/(default)/secret-santa/admin/+page.svelte
+++ src/routes/(default)/secret-santa/admin/+page.svelte
@@ -14,20 +14,28 @@
 
 <script lang="ts">
     import api from "$lib/api";
+    import A from "$lib/components/A.svelte";
     import Icon from "$lib/components/Icon.svelte";
     import Loading from "$lib/components/Loading.svelte";
     import Mention from "$lib/components/Mention.svelte";
     import Timestamp from "$lib/components/Timestamp.svelte";
     import UserId from "$lib/components/UserId.svelte";
     import UserMention from "$lib/components/UserMention.svelte";
-    import { token } from "$lib/stores";
+    import { token, user } from "$lib/stores";
     import type { SecretSantaUser } from "$lib/types";
     import { onMount } from "svelte";
 
+    let loading = true;
     let entries: SecretSantaUser[];
+    let users: string[];
 
     async function load() {
+        loading = true;
+
+        if ($user!.observer) users = await api($token, `GET /secret-santa/admin/admins`).catch(alert);
         entries = await api($token, `GET /secret-santa/all`).catch(alert);
+
+        loading = false;
     }
 
     onMount(load);
@@ -37,11 +45,39 @@
         await api($token, `POST /secret-santa/admin/${key}/${id}`).catch(alert);
         await load();
     }
+
+    async function addAdmin() {
+        const id = prompt("Enter a user ID.");
+        if (!id) return;
+
+        await api($token, `PUT /secret-santa/admin/admins/${id}`).catch(alert);
+        await load();
+    }
+
+    async function rmAdmin(id: string) {
+        if (!confirm("Remove admin?")) return;
+
+        await api($token, `DELETE /secret-santa/admin/admins/${id}`).catch(alert);
+        await load();
+    }
 </script>
 
 <h2>Secret Santa Admin Panel</h2>
 <p><button class="row" on:click={load}><Icon icon="refresh" /> Reload</button></p>
-<Loading done={entries}>
+<Loading done={!loading}>
+    {#if $user?.observer}
+        <h3>Admins</h3>
+        <ul class="space">
+            <li><b>All Observers</b></li>
+            {#each users as user}
+                <li>
+                    <span class="row"><UserMention id={user} /> &mdash; <A class="row red-text" on:click={() => rmAdmin(user)}><Icon icon="delete" /></A></span>
+                </li>
+            {/each}
+        </ul>
+        <br />
+        <p><button on:click={addAdmin}><Icon icon="add" /></button></p>
+    {/if}
     <h3><b>Pool size:</b> {entries.filter((x) => x.status === "pool-free").length}</h3>
     {#each entries.sort((x, y) => (x.status === "awaiting-approval" ? 0 : 1) - (y.status === "awaiting-approval" ? 0 : 1)) as entry}
         <div class="panel" style={entry.status === "awaiting-approval" ? "outline: 2px solid rgb(var(--text-accent))" : ""}>
