c86c637783eca65205f14d6c7f06b438fa014d29
hyper-neutrino <alex.yj.liao@gmail.com>
2024-02-14 09:16:47 -0500

    restrict short links to council members

--- src/routes/(default)/tools/url-shortener/+layout.svelte
+++ src/routes/(default)/tools/url-shortener/+layout.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
     import { page } from "$app/stores";
+    import Callout from "$lib/components/Callout.svelte";
     import Login from "$lib/components/Login.svelte";
     import { user } from "$lib/stores";
 </script>
 
 {#if !$user}
     <Login redirect={$page.url.pathname} />
+{:else if !$user.council}
+    <div class="container">
+        <Callout style="red">
+            <p>This feature is only available to council members.</p>
+        </Callout>
+    </div>
 {:else}
     <slot />
 {/if}
